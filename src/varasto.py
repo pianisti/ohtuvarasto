2613cac275e8024ae788e200412c91cd30d07554
pianisti <192371238+pianisti@users.noreply.github.com>
2025-11-13 00:53:09 +0200

    korjataan virhe

--- src/varasto.py
+++ src/varasto.py
@@ -1,18 +1,15 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-
+        self.tilavuus = max(tilavuus, 0)
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
-        self.saldo = tilavuus
+        else:
+            # täyteen ja ylimäärä hukkaan!
+            self.saldo = tilavuus
 
     # huom: ominaisuus voidaan myös laskea.
     def paljonko_mahtuu(self):
